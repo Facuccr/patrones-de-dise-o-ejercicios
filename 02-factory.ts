e2aabd9c13a3db86af510a5628edb337c1a1b61e
FacuuC <facundocristaldo146@gmail.com>
2026-08-25 11:31:51 -0300

    implementar fabrica y concluir ejercicio

--- 02-factory.ts
+++ 02-factory.ts
@@ -34,3 +34,24 @@ class Servidor extends Equipo {
     }
 }
 
+
+class EquipoFactory{
+    crearEquipo(tipo: string,nombre: string,ram:string,procesador: string): Equipo{ 
+        switch (tipo.toLowerCase()) {
+            case 'notebook':
+                return new Notebook(nombre, ram, procesador);
+            case 'desktop':
+                return new Desktop(nombre, ram, procesador);
+            case 'servidor':
+                return new Servidor(nombre, ram, procesador);
+            default:
+                throw new Error("Tipo de equipo no soportado");
+        }
+    }
+}
+
+
+const factory = new EquipoFactory();
+const unaNotebook = factory.crearEquipo("Notebook", "Dell XPS", "16GB", "i7");
+console.log(unaNotebook.detalles());
+// Tipo: Notebook, Nombre: Dell XPS, RAM: 16GB, Procesador: i7
