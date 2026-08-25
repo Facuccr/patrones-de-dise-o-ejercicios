cd75d1f5c990042bf703fea3cb46e582ba445e61
FacuuC <facundocristaldo146@gmail.com>
2026-08-25 11:52:21 -0300

    crear el soporte usando la interfaz

--- 03-observer.ts
+++ 03-observer.ts
@@ -1,3 +1,24 @@
 interface Observador {
     notificar(nombreEquipo: string, nuevoEstado: string): void;
 }
+
+
+class Soporte implements Observador{
+    notificar(nombreEquipo: string, nuevoEstado: string): void{
+        console.log(`Soporte notificado: ${nombreEquipo} ha cambiado su estado a ${nuevoEstado}`);
+    }
+}
+
+class Equipo {
+    nombre: string;
+    tipo: string;
+    estado: string;
+    private observadores: Observador[] = []
+
+
+    constructor(nombre: string, tipo: string, estado: string) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.estado = estado;
+    }s
+}
